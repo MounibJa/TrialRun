32c993561b74636abc983411c7b6e04c36bb7252
MounibJa <149970700+MounibJa@users.noreply.github.com>
2025-10-01 14:05:57 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -29,7 +29,7 @@ async def test_tt_um_example(dut):
     # Loading a value (load=1, OE=0)
     dut.ui_in.value = 0b01
     dut.uio_in.value = 101
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 2)
     assert dut.uio_out.value == 101, f"value should 101, got {int(dut.uio_out.value)}"
     assert dut.uo_out.value == 0, f"OE=0 so output should be 0, got {int(dut.uo_out.value)}"
     assert dut.uio_oe.value == 0, f"OE pin should be 0, got {int(dut.uio_oe.value)}"
