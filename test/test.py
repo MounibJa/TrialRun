3abb4e88a5734ff1697773b8bfbd35b4b1e98f1c
MounibJa <149970700+MounibJa@users.noreply.github.com>
2025-10-01 13:55:45 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -33,9 +33,9 @@ async def test_tt_um_example(dut):
     dut.ui_in.value = 0b01   # load=1, OE=1
     dut.uio_in.value = 101
     await ClockCycles(dut.clk, 1)
-    assert dut.uio_out.value == 101
-    assert dut.uo_out.value == 0  # OE=0, output should be 0
-    assert dut.uio_oe.value == 0  
+    assert dut.uio_out.value == 101, f"value should 101, got {int(dut.uo_out.value)}"
+    assert dut.uo_out.value == 0, f" got {int(dut.uio_out.value)}"  # OE=0, output should be 0
+    assert dut.uio_oe.value == 0  f"got {int(dut.uio_oe.value)}"
     dut._log.info("test passed for loading a value when oe is on and when its off")
     
     # no load, start incrementing but still oe off
