17349f2abc99402ecfa5433f555fbebdd13e09f3
MounibJa <149970700+MounibJa@users.noreply.github.com>
2025-10-01 13:44:11 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,43 +1,37 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire [7:0] ui_in,    // [0]=load, [1]=OE
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
 
-    
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
 );
+
     reg [7:0] counts;
 
-    wire out_ena = ui_in[0];
+    wire load     = ui_in[0]; // load enable
+    
+    wire oecrtl = ui_in[1]; // crtl if oe is being toggled or not
     
     always @(negedge rst_n or posedge clk) begin
         if(!rst_n) begin
-            counts<= 8'd0;
-        end  else if( out_ena) begin
-            counts <= counts+ 1'd1;
-            
+            counts <= 8'd0;
+        end else if(load & !oecrtl) begin
+            counts <= uio_in;       // load the value
+        end else begin
+            counts <= counts + 1;   // increment if no load
         end
     end
-    
-  // All output pins must be assigned. If not used, assign to 0.
 
-    
-  assign uio_out = counts;
-  assign uio_oe  = {8{out_ena}};
-  assign uo_out = out_ena ? counts : 8'b0;
+    assign uio_out = counts;
+    assign uio_oe  = {8{oecrtl}};    // drive UIO only when OE=1
+    assign uo_out  = oecrtl ? counts : 8'b0;
 
-  // List all unused inputs to prevent warnings
-    wire _unused = &{ena, uio_in, ui_in[7:1], 1'b0};
+    wire _unused = &{ena, ui_in[7:2], 1'b0};
 
 endmodule
